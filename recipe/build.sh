0fa9784036357cea512b6097af36872bc99b7596
Filipe Fernandes <ocefpaf@gmail.com>
2018-09-03 19:21:04 -0300

    remove .la

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,8 +2,11 @@
 
 bash configure --prefix=$PREFIX --disable-all-programs --enable-libuuid
 
-make
+make -j${CPU_COUNT} ${VERBOSE_AT}
 make tests
 make install
 
 rm -fr $PREFIX/share
+
+# We can remove this when we start using the new conda-build.
+find $PREFIX -name '*.la' -delete
